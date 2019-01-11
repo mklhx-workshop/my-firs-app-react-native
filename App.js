ff2b47f6046e42974c795a0a55e9ca510f3e913e
MkLHX <mickael.lehoux@gmail.com>
2019-01-11 17:00:55 +0100

    accelerometer app

--- App.js
+++ App.js
@@ -1,50 +1,102 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Accelerometer from 'react-native-sensors';
-import Vibration from 'react-native-sensors';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Accelerometer } from 'expo';
 
-const Value = ({name, value}) => (
-  <View style={styles.valueContainer}>
-    <Text style={styles.valueName}>{name}:</Text>
-    <Text style={styles.valueValue}>{new String(value).substr(0, 8)}</Text>
-  </View>
-)
+export default class AccelerometerSensor extends React.Component {
+  state = {
+    accelerometerData: {},
+  }
 
-export default class App extends React.Component {
+  componentDidMount() {
+    this._toggle();
+  }
 
-  constructor(props) {
-    super(props);
+  componentWillUnmount() {
+    this._unsubscribe();
+  }
 
-    new Accelerometer({
-      updateInterval: 400 // defaults to 100ms
-    })
-      .then(observable => {
-        observable.subscribe(({ x, y, z }) => this.setState({ x, y, z }));
-      })
-      .catch(error => {
-        console.log("The sensor is not available");
-      });
+  _toggle = () => {
+    if (this._subscription) {
+      this._unsubscribe();
+    } else {
+      this._subscribe();
+    }
+  }
 
-    this.state = { x: 0, y: 0, z: 0 };
+  _slow = () => {
+    Accelerometer.setUpdateInterval(1000);
+  }
+
+  _fast = () => {
+    Accelerometer.setUpdateInterval(16);
+  }
+
+  _subscribe = () => {
+    this._subscription = Accelerometer.addListener(accelerometerData => {
+      this.setState({ accelerometerData });
+    });
+  }
+
+  _unsubscribe = () => {
+    this._subscription && this._subscription.remove();
+    this._subscription = null;
   }
 
   render() {
+    let { x, y, z } = this.state.accelerometerData;
+
     return (
-      <View style={styles.container}>
-        <Text>This is my first app on react-native and i play with accelerometer!</Text>
-        <Value name="x" value={this.state.X}/>
-        <Value name="y" value={this.state.Y}/>
-        <Value name="z" value={this.state.Z}/>
+      <View style={styles.sensor}>
+        <Text>Accelerometer:</Text>
+        <Text>x: {round(x)} y: {round(y)} z: {round(z)}</Text>
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity onPress={this._toggle} style={styles.button}>
+            <Text>Toggle</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this._slow} style={[styles.button, styles.middleButton]}>
+            <Text>Slow</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this._fast} style={styles.button}>
+            <Text>Fast</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
 }
 
+function round(n) {
+  if (!n) {
+    return 0;
+  }
+
+  return Math.floor(n * 100) / 100;
+}
+
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    marginTop: 15,
+  },
+  button: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#eee',
+    padding: 10,
+  },
+  middleButton: {
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderColor: '#ccc',
+  },
+  sensor: {
+    marginTop: 30,
+    paddingHorizontal: 10,
   },
 });
